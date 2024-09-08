46a3b78c3f765b063855d99acdcb9ee56d872970
Sambit Mondal <sambitmondal2005@gmail.com>
2024-09-08 11:46:58 +0530

    Minor functionality update

--- next-app/src/app/api/login/route.ts
+++ next-app/src/app/api/login/route.ts
@@ -1,9 +1,9 @@
 import { connectDB, UserModel } from '../../../../database/database';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(req: NextResponse) {
+export async function POST(req: NextRequest) {
     try {
         const { email, password } = await req.json();
 
